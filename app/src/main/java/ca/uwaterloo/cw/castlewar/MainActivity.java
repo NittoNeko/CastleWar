69493c143e166a5beb8ba9744dd54208055bf42c
NittoNeko <harrison_bz@Hotmail.com>
2018-02-04 15:18:21 -0500

    update

--- app/src/main/java/ca/uwaterloo/cw/castlewar/MainActivity.java
+++ app/src/main/java/ca/uwaterloo/cw/castlewar/MainActivity.java
@@ -10,4 +10,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void enterSinglePlayer(View view)
+    {
+        // Create an intent to show a new screen passing data to it
+        Intent userIntent = new Intent(this, PrepareActivity.class);
+
+        // Show new screen
+        startActivity(userIntent);
+    }
+
+    public void enterShop(View view)
+    {
+        // Create an intent to show a new screen passing data to it
+        Intent userIntent = new Intent(this, ShopAcitivity.class);
+
+        // Show new screen
+        startActivity(userIntent);
+    }
+
+    public void enterInventory(View view)
+    {
+        // Create an intent to show a new screen passing data to it
+        Intent userSearchIntent = new Intent(this, InventoryActivity.class);
+
+        // Show new screen
+        startActivity(userIntent);
+    }
 }
