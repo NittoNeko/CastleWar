5b2eb48cec96b3f9194751147b7586f888f25ca6
Yehao Wang <Wang.Yehao.SH@Gmail.com>
2018-02-14 19:20:01 -0500

    Correct the mistake result from adding land layout.

--- app/src/main/java/ca/uwaterloo/cw/castlewar/MainActivity.java
+++ app/src/main/java/ca/uwaterloo/cw/castlewar/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
-import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,9 +16,6 @@ public class MainActivity extends AppCompatActivity {
         // Set fullscreen
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        // Set No Title
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 
     /*public void enterSinglePlayer(View view)
